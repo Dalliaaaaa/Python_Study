c2b7a311848f442ead6d8b32253da657e45a22e9
Dalliaaaaa <110238987+Dalliaaaaa@users.noreply.github.com>
2022-09-07 23:29:11 +0900

    Create range.py

---
+++ 프로그래밍 및 실습/range.py
@@ -0,0 +1,12 @@
+# sum = 0
+# for i in [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+#   sum += i
+# print(sum)
+
+sum = 0
+for i in range(1, 11): #1부터 10까지 반복
+  sum += i
+print(sum)
+
+for e in range(3) : #첫 번째 0 생략 가능
+  print("Happy")
