b3a73e4c761e4f85ad93254752509f5b7f42afd8
Dalliaaaaa <110238987+Dalliaaaaa@users.noreply.github.com>
2022-09-09 23:49:53 +0900

    Create in_or_notin.py

---
+++ 프로그래밍 및 실습/in_or_notin.py
@@ -0,0 +1,15 @@
+s = "Tomato spaghetti"
+
+if s.find("ghe") != -1:
+  print("있습니다.")
+else:
+  print("없습니다.")
+
+  #s.find("ghe")는 10을 return
+  #10은 문자열 s에서 첫문자 g가 위치한 index 값을 return
+  #ghe가 문자열 s에 존재하지 않는다면 -1을 return
+
+# if "ghe" in s:
+#   print("있습니다.")
+# else:
+#   print("없습니다.")
